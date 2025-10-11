9ecf76284a1d87c970d9ac74100adfda83462b16
ziran <gfgfgfggf02@gmail.com>
2025-10-11 10:52:56 +0300

    add autogen4

--- src/main/java/com/zircky/rotarycraft_unofficial/common/data/tools/RCUTools.java
+++ src/main/java/com/zircky/rotarycraft_unofficial/common/data/tools/RCUTools.java
@@ -19,41 +19,162 @@ public class RCUTools {
     RCURegistrates.REGISTRATE.creativeModeTab(() -> RCUCreativeModeTabs.TOOLS);
   }
 
-  public static ItemEntry<Item> Screwdriver = RCURegistrates.REGISTRATE.item("screwdriver", Item::new).lang("Screwdriver").defaultModel().register();
-  public static ItemEntry<Item> AngularTransducer = RCURegistrates.REGISTRATE.item("angular_transducer", Item::new).lang("Angular Transducer").defaultModel().register();
-  public static final ItemEntry<SpringItem> WIND_SPRING = RCURegistrates.REGISTRATE.item("wind_spring", props -> new SpringItem("Wind Spring", props.stacksTo(1))).register();
+  public static ItemEntry<Item> Screwdriver = RCURegistrates.REGISTRATE
+      .item("screwdriver", Item::new).lang("Screwdriver")
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/screwdriver")))
+      .register();
 
-  public static final ItemEntry<SpringItem> HIGH_STRENGTH_SPRING = RCURegistrates.REGISTRATE.item("high_strength_spring", props -> new SpringItem("High-Strength Spring", props.stacksTo(1))).register();
+  public static ItemEntry<Item> AngularTransducer = RCURegistrates.REGISTRATE
+      .item("angular_transducer", Item::new).lang("Angular Transducer")
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/angular_transducer")))
+      .register();
 
-  public static final ItemEntry<EnergyItem> Ultrasound = RCURegistrates.REGISTRATE.item("ultrasound", props -> new EnergyItem("Ultrasound", props.stacksTo(1), 320000)).register();
-  public static final ItemEntry<EnergyItem> MotionTracker = RCURegistrates.REGISTRATE.item("motion_tracker", props -> new EnergyItem("Motion Tracker", props.stacksTo(1), 320000)).register();
-  public static final ItemEntry<EnergyItem> Vacuum = RCURegistrates.REGISTRATE.item("vacuum", props -> new EnergyItem("Vacuum", props.stacksTo(1), 320000)).register();
-  public static final ItemEntry<EnergyItem> KnockbackGun = RCURegistrates.REGISTRATE.item("knockback_gun", props -> new EnergyItem("Knockback Gun", props.stacksTo(1), 320000)).register();
-  public static final ItemEntry<EnergyItem> GravelGun = RCURegistrates.REGISTRATE.item("gravel_gun", props -> new EnergyItem("Gravel Gun", props.stacksTo(1), 320000, "Dealing 1142.6 hearts of damage per shot")).register();
+  public static final ItemEntry<SpringItem> WIND_SPRING = RCURegistrates.REGISTRATE
+      .item("wind_spring", props -> new SpringItem("Wind Spring", props.stacksTo(1)))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/wind_spring")))
+      .register();
 
-  public static final ItemEntry<BedrockPickaxeItem> BedrockPickaxe = RCURegistrates.REGISTRATE.item("bedrock_pickaxe", props -> new BedrockPickaxeItem()).register();
-  public static final ItemEntry<BedrockAxeItem> BedrockAxe = RCURegistrates.REGISTRATE.item("bedrock_axe", props -> new BedrockAxeItem()).register();
-  public static final ItemEntry<BedrockShovelItem> BedrockShovel = RCURegistrates.REGISTRATE.item("bedrock_shovel", props -> new BedrockShovelItem()).register();
-  public static final ItemEntry<BedrockHoeItem> BedrockHoe = RCURegistrates.REGISTRATE.item("bedrock_hoe", props -> new BedrockHoeItem()).register();
-  public static final ItemEntry<BedrockSwordItem> BedrockSword = RCURegistrates.REGISTRATE.item("bedrock_sword", props -> new BedrockSwordItem()).register();
-  public static final ItemEntry<ItemRailGunAmmo> RailGunAmmo = RCURegistrates.REGISTRATE.item("rail_gun_ammo", ItemRailGunAmmo::new).register();
-  public static final ItemEntry<BedrockArmor> BedrockHelmet = RCURegistrates.REGISTRATE.item("bedrock_helmet", props -> new BedrockArmor("Bedrock Helmet", RCUArmorMaterials.BEDROCK, ArmorItem.Type.HELMET, props)).register();
-  public static final ItemEntry<BedrockArmor> BedrockChestplate = RCURegistrates.REGISTRATE.item("bedrock_chestplate", props -> new BedrockArmor("Bedrock Chestplate", RCUArmorMaterials.BEDROCK, ArmorItem.Type.CHESTPLATE, props)).register();
-  public static final ItemEntry<BedrockArmor> BedrockLeggings = RCURegistrates.REGISTRATE.item("bedrock_leggings", props -> new BedrockArmor("Bedrock Leggings", RCUArmorMaterials.BEDROCK, ArmorItem.Type.LEGGINGS, props)).register();
-  public static final ItemEntry<BedrockArmor> BedrockBoots = RCURegistrates.REGISTRATE.item("bedrock_boots", props -> new BedrockArmor("Bedrock Boots", RCUArmorMaterials.BEDROCK, ArmorItem.Type.BOOTS, props)).register();
-  public static final ItemEntry<BedrockArmor> NVGoggles = RCURegistrates.REGISTRATE.item("nv_goggles", props -> new BedrockArmor("Night Vision Goggles", RCUArmorMaterials.NVGM, ArmorItem.Type.HELMET, props)).register();
-  public static final ItemEntry<BedrockArmor> IOGoggles = RCURegistrates.REGISTRATE.item("io_goggles", props -> new BedrockArmor("I/O Goggles", RCUArmorMaterials.IOGM, ArmorItem.Type.HELMET, props)).register();
-  public static final ItemEntry<CustemItem> PerojectorSlide = RCURegistrates.REGISTRATE.item("perojector_slide", props -> new CustemItem("Perojector Slide", props.stacksTo(1))).register();
-  public static final ItemEntry<HSLASteelAxeItem> HslaSteelAxe = RCURegistrates.REGISTRATE.item("hsla_steel_axe", props -> new HSLASteelAxeItem("Hsla Steel Axe")).register();
-  public static final ItemEntry<HSLASteelAxeItem> HslaSteelHoe = RCURegistrates.REGISTRATE.item("hsla_steel_hoe", props -> new HSLASteelAxeItem("Hsla Steel Hoe")).register();
-  public static final ItemEntry<HSLASteelAxeItem> HslaSteelPickaxe = RCURegistrates.REGISTRATE.item("hsla_steel_pickaxe", props -> new HSLASteelAxeItem("Hsla Steel Pickaxe")).register();
-  public static final ItemEntry<HSLASteelAxeItem> HslaSteelShovel = RCURegistrates.REGISTRATE.item("hsla_steel_shovel", props -> new HSLASteelAxeItem("Hsla Steel Shovel")).register();
-  public static final ItemEntry<HSLASteelAxeItem> HslaSteelSword = RCURegistrates.REGISTRATE.item("hsla_steel_sword", props -> new HSLASteelAxeItem("Hsla Steel Sword")).register();
-  public static final ItemEntry<HSLASteelArmor> HslaSteelHelmet = RCURegistrates.REGISTRATE.item("hsla_steel_helmet", props -> new HSLASteelArmor("Hsla Steel Helmet", RCUArmorMaterials.HSLA, ArmorItem.Type.HELMET, props)).register();
-  public static final ItemEntry<HSLASteelArmor> HslaSteelChestplate = RCURegistrates.REGISTRATE.item("hsla_steel_chestplate", props -> new HSLASteelArmor("Hsla Steel Chestplate", RCUArmorMaterials.HSLA, ArmorItem.Type.CHESTPLATE, props)).register();
-  public static final ItemEntry<HSLASteelArmor> HslaSteelLeggings = RCURegistrates.REGISTRATE.item("hsla_steel_leggings", props -> new HSLASteelArmor("Hsla Steel Leggings", RCUArmorMaterials.HSLA, ArmorItem.Type.LEGGINGS, props)).register();
-  public static final ItemEntry<HSLASteelArmor> HslaSteelBoots = RCURegistrates.REGISTRATE.item("hsla_steel_boots", props -> new HSLASteelArmor("Hsla Steel Boots", RCUArmorMaterials.HSLA, ArmorItem.Type.BOOTS, props)).register();
+  public static final ItemEntry<SpringItem> HIGH_STRENGTH_SPRING = RCURegistrates.REGISTRATE
+      .item("high_strength_spring", props -> new SpringItem("High-Strength Spring", props.stacksTo(1)))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/high_strength_spring")))
+      .register();
+
+  public static final ItemEntry<EnergyItem> Ultrasound = RCURegistrates.REGISTRATE
+      .item("ultrasound", props -> new EnergyItem("Ultrasound", props.stacksTo(1), 320000))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/ultrasound")))
+      .register();
+
+  public static final ItemEntry<EnergyItem> MotionTracker = RCURegistrates.REGISTRATE
+      .item("motion_tracker", props -> new EnergyItem("Motion Tracker", props.stacksTo(1), 320000))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/motion_tracker")))
+      .register();
+
+  public static final ItemEntry<EnergyItem> Vacuum = RCURegistrates.REGISTRATE
+      .item("vacuum", props -> new EnergyItem("Vacuum", props.stacksTo(1), 320000))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/vacuum")))
+      .register();
+
+  public static final ItemEntry<EnergyItem> KnockbackGun = RCURegistrates.REGISTRATE
+      .item("knockback_gun", props -> new EnergyItem("Knockback Gun", props.stacksTo(1), 320000))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/knockback_gun")))
+      .register();
+
+  public static final ItemEntry<EnergyItem> GravelGun = RCURegistrates.REGISTRATE
+      .item("gravel_gun", props -> new EnergyItem("Gravel Gun", props.stacksTo(1), 320000, "Dealing 1142.6 hearts of damage per shot"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/gravel_gun")))
+      .register();
+
+  public static final ItemEntry<BedrockPickaxeItem> BedrockPickaxe = RCURegistrates.REGISTRATE
+      .item("bedrock_pickaxe", props -> new BedrockPickaxeItem())
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_pickaxe")))
+      .register();
+
+  public static final ItemEntry<BedrockAxeItem> BedrockAxe = RCURegistrates.REGISTRATE
+      .item("bedrock_axe", props -> new BedrockAxeItem())
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_axe")))
+      .register();
+
+  public static final ItemEntry<BedrockShovelItem> BedrockShovel = RCURegistrates.REGISTRATE
+      .item("bedrock_shovel", props -> new BedrockShovelItem())
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_shovel")))
+      .register();
+
+  public static final ItemEntry<BedrockHoeItem> BedrockHoe = RCURegistrates.REGISTRATE
+      .item("bedrock_hoe", props -> new BedrockHoeItem())
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_hoe")))
+      .register();
+
+  public static final ItemEntry<BedrockSwordItem> BedrockSword = RCURegistrates.REGISTRATE
+      .item("bedrock_sword", props -> new BedrockSwordItem())
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_sword")))
+      .register();
+
+  public static final ItemEntry<ItemRailGunAmmo> RailGunAmmo = RCURegistrates.REGISTRATE
+      .item("rail_gun_ammo", ItemRailGunAmmo::new)
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/rail_gun_ammo_0")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> BedrockHelmet = RCURegistrates.REGISTRATE
+      .item("bedrock_helmet", props -> new BedrockArmor("Bedrock Helmet", RCUArmorMaterials.BEDROCK, ArmorItem.Type.HELMET, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_helmet")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> BedrockChestplate = RCURegistrates.REGISTRATE
+      .item("bedrock_chestplate", props -> new BedrockArmor("Bedrock Chestplate", RCUArmorMaterials.BEDROCK, ArmorItem.Type.CHESTPLATE, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_chestplate")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> BedrockLeggings = RCURegistrates.REGISTRATE
+      .item("bedrock_leggings", props -> new BedrockArmor("Bedrock Leggings", RCUArmorMaterials.BEDROCK, ArmorItem.Type.LEGGINGS, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_leggings")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> BedrockBoots = RCURegistrates.REGISTRATE
+      .item("bedrock_boots", props -> new BedrockArmor("Bedrock Boots", RCUArmorMaterials.BEDROCK, ArmorItem.Type.BOOTS, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/bedrock/bedrock_boots")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> NVGoggles = RCURegistrates.REGISTRATE
+      .item("nv_goggles", props -> new BedrockArmor("Night Vision Goggles", RCUArmorMaterials.NVGM, ArmorItem.Type.HELMET, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/nv_goggles")))
+      .register();
+
+  public static final ItemEntry<BedrockArmor> IOGoggles = RCURegistrates.REGISTRATE
+      .item("io_goggles", props -> new BedrockArmor("I/O Goggles", RCUArmorMaterials.IOGM, ArmorItem.Type.HELMET, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/io_goggles")))
+      .register();
+
+  public static final ItemEntry<CustemItem> PerojectorSlide = RCURegistrates.REGISTRATE
+      .item("perojector_slide", props -> new CustemItem("Perojector Slide", props.stacksTo(1)))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/perojector_slide")))
+      .register();
+
+  public static final ItemEntry<HSLASteelAxeItem> HslaSteelAxe = RCURegistrates.REGISTRATE
+      .item("hsla_steel_axe", props -> new HSLASteelAxeItem("Hsla Steel Axe"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_axe")))
+      .register();
+
+  public static final ItemEntry<HSLASteelAxeItem> HslaSteelHoe = RCURegistrates.REGISTRATE
+      .item("hsla_steel_hoe", props -> new HSLASteelAxeItem("Hsla Steel Hoe"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_hoe")))
+      .register();
+
+  public static final ItemEntry<HSLASteelAxeItem> HslaSteelPickaxe = RCURegistrates.REGISTRATE
+      .item("hsla_steel_pickaxe", props -> new HSLASteelAxeItem("Hsla Steel Pickaxe"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_pickaxe")))
+      .register();
+
+  public static final ItemEntry<HSLASteelAxeItem> HslaSteelShovel = RCURegistrates.REGISTRATE
+      .item("hsla_steel_shovel", props -> new HSLASteelAxeItem("Hsla Steel Shovel"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_shovel")))
+      .register();
+
+  public static final ItemEntry<HSLASteelAxeItem> HslaSteelSword = RCURegistrates.REGISTRATE
+      .item("hsla_steel_sword", props -> new HSLASteelAxeItem("Hsla Steel Sword"))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_sword")))
+      .register();
+
+  public static final ItemEntry<HSLASteelArmor> HslaSteelHelmet = RCURegistrates.REGISTRATE
+      .item("hsla_steel_helmet", props -> new HSLASteelArmor("Hsla Steel Helmet", RCUArmorMaterials.HSLA, ArmorItem.Type.HELMET, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_helmet")))
+      .register();
+
+  public static final ItemEntry<HSLASteelArmor> HslaSteelChestplate = RCURegistrates.REGISTRATE
+      .item("hsla_steel_chestplate", props -> new HSLASteelArmor("Hsla Steel Chestplate", RCUArmorMaterials.HSLA, ArmorItem.Type.CHESTPLATE, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_chestplate")))
+      .register();
+
+  public static final ItemEntry<HSLASteelArmor> HslaSteelLeggings = RCURegistrates.REGISTRATE
+      .item("hsla_steel_leggings", props -> new HSLASteelArmor("Hsla Steel Leggings", RCUArmorMaterials.HSLA, ArmorItem.Type.LEGGINGS, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_leggings")))
+      .register();
+
+  public static final ItemEntry<HSLASteelArmor> HslaSteelBoots = RCURegistrates.REGISTRATE
+      .item("hsla_steel_boots", props -> new HSLASteelArmor("Hsla Steel Boots", RCUArmorMaterials.HSLA, ArmorItem.Type.BOOTS, props))
+      .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/tools/hsla/hsla_steel_boots")))
+      .register();
 
 
-  public static void init() {}
+  public static void init() {
+  }
 }
