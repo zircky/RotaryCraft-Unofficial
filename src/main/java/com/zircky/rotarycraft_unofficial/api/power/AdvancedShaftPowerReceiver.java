eef8b15fc6c8d5c4b79015dd4edbb51840a1a8e5
ziran <gfgfgfggf02@gmail.com>
2025-10-11 10:41:01 +0300

    add engine

---
+++ src/main/java/com/zircky/rotarycraft_unofficial/api/power/AdvancedShaftPowerReceiver.java
@@ -0,0 +1,5 @@
+package com.zircky.rotarycraft_unofficial.api.power;
+
+public interface AdvancedShaftPowerReceiver extends PowerAcceptor {
+//  public boolean addPower(int torque, int omega, long power, ForgeDirection side);
+}
